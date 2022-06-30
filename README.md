b60c1a276731ad25ab0af4c3ad2da5d0e5f9c238
huckjuhwang <skygurwn96@naver.com>
2022-06-30 23:22:28 +0900

    study start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kotlin_study
 코틀린 기본문법을 학습하기 위한 레파지토리입니다.
+
+
+var vs val
+
+var = 수정가능한 변수
+val(final) = 수정 불가능한 변수
---
+++ src/lec01/lec01Main.kt
@@ -0,0 +1,12 @@
+package lec01
+
+
+fun main(){
+    // variable
+    var number1: Long = 10L
+    number1 = 4L
+
+//    Val cannot be reassigned!!!
+    val number2: Long = 10L
+//    number2 = 5L
+}
