a069d67d5864910f87d9dd45d0830f811cef6cd0
huckjuhwang <skygurwn96@naver.com>
2022-06-30 23:34:00 +0900

    lec01

--- src/lec01/lec01Main.kt
+++ src/lec01/lec01Main.kt
@@ -9,4 +9,14 @@ fun main(){
 //    Val cannot be reassigned!!!
     val number2: Long = 10L
 //    number2 = 5L
+
+    // null값을 넣어주기 위해 ?를 사용할 수 있다.
+    var number3: Long? = 10000
+    number3 = null
+
+    var person = Person("name")
 }
+
+data class Person(
+    var name: String,
+)
