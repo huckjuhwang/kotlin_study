a55a661639de5090a1aa50ee9c1be5594eb54f02
huckjuhwang <skygurwn96@naver.com>
2022-07-01 00:53:54 +0900

    lec03

---
+++ src/lec01/lec03Main.kt
@@ -0,0 +1,10 @@
+package lec01
+
+fun main(){
+    var number1 = 3 // Int
+//    var number2: Long = number1 // Type mismatch!!
+
+    var number3: Long = number1.toLong()
+    println(number3)
+}
+
